3f39aeca7e72fe80881ca4aca85497470476a23b
CoderEric04 <94086390+CoderEric04@users.noreply.github.com>
2023-08-20 03:32:19 +0100

    Fixed stuff.

--- Makefile
+++ Makefile
@@ -1,7 +1,6 @@
 CFLAGS=-std=c17 -Wall -Wextra -Werror
-LIB_DIR=src/lib
-LIBS=-lmingw32 -lSDL2main -lSDL2
-INCLUDE_DIR=src/include
-all: chip8
-chip8: chip8.c
-	gcc chip8.c -o chip8 $(CFLAGS) -L$(LIB_DIR) $(LIBS) -I$(INCLUDE_DIR)
+LIBS=C:\Users\erict\Desktop\CHIP8-Emulator\src\lib -lmingw32 -lSDL2main -lSDL2
+INCLUDES=C:\Users\erict\Desktop\CHIP8-Emulator\src\include
+
+all:
+	gcc chip8.c -o chip8 $(CFLAGS) -L$(LIBS) -I$(INCLUDES)
--- chip8.c
+++ chip8.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
-#include "SDL2/SDL.h"
+#include <SDL2/SDL.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char ** arg) {
     (void)argc;
-    (void)argv;
+    (void)arg;
 
-    printf("TEST");
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window * win = SDL_CreateWindow("title", 30, 30, 600, 500, SDL_WINDOW_SHOWN);
+
+    SDL_Surface * screen = SDL_GetWindowSurface(win);
+    SDL_Delay(2000); 
+    SDL_DestroyWindow(win); 
+    SDL_Quit();
+
 
     return 0;
 }
